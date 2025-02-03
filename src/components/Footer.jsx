155af8b40c6847d8c396ae9363828aa7ad9d99ab
irakli <irakli.kasradze.1@btu.edu.ge>
2025-02-03 22:19:44 +0400

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import styles from '../styles/Footer.module.css';
 
 function Footer() {
-  return <footer className={styles.footer}>&copy; {new Date().getFullYear()} Made For Front-End React Final Project</footer>;
-};
+  return (
+    <footer className={styles.footer}>
+      &copy; {new Date().getFullYear()} Made For Front-End React Final Project. API used: 
+      <a href="https://rawg.io/apidocs" target="_blank" rel="noopener noreferrer" className={styles.link}>
+        RAWG
+      </a>
+    </footer>
+  );
+}
 
 export default Footer;
