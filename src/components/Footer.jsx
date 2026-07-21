997cd9a7d26127f25ad3cc874319ac6797a83675
Irakli-ka <irakli.kasradze.1@btu.edu.ge>
2026-07-21 21:08:03 +0400

    change footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      &copy; {new Date().getFullYear()} Made For Front-End React Final Project. API used: 
+      &copy; {new Date().getFullYear()} API used: 
       <a href="https://rawg.io/apidocs" target="_blank" rel="noopener noreferrer" className={styles.link}>
         RAWG
       </a>
